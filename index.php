325510519ece4cf353eafe957fadc51fb5f47424
William Entriken <github.com@phor.net>
2016-02-20 13:54:19 -0500

    update bootstrap

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $project_count = 0;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $config->siteName ?></title>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
